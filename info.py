bd9a60fa59fe71dd6b1a9d88a91230626cf6632e
namanathate220212-design <namanathate220212@gmail.com>
2026-04-29 20:27:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,8 +11,8 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup 
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
+API_ID = int(environ['30813743'])
+API_HASH = environ['ab49a25cc9768bd163e7b3844c95057a']
 BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
@@ -32,8 +32,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://namanathate220212_db_user:<*Naman_220212>@skyflixprimebot.7cbfikg.mongodb.net/?appName=SkyFlixPrimeBot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "SkyFlixPrimeBot")
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
 FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
